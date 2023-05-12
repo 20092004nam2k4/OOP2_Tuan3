c78f75f0fdff08fd946812f23e95bacaffb2d140
20092004nam2k4 <114391153+20092004nam2k4@users.noreply.github.com>
2023-05-12 12:00:53 +0700

    thuchanh

---
+++ thuchanh5.java
@@ -0,0 +1,22 @@
+package thuhanh;
+
+public class thuchanh5 {
+  static void fun() {
+    try {
+      throw new NullPointerException ( "demo" );
+    } catch (NullPointerException e) {
+      System.out.println ( "Caught inside fun()." );
+      throw e; // rethrowing the exception
+    }
+  }
+
+  public static void main(String args[]) {
+    try {
+      fun ( );
+    } catch (NullPointerException e) {
+      System.out.println ( "Caught in main." );
+    }
+  }
+}
+
+
